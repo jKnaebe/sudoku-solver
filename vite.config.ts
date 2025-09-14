5d2c988bac0b9c8e34292661c24c23caf569facb
Jan Knaebe <jm.knaebe@gmail.com>
2025-09-14 13:59:37 +0200

    changed the base

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/sudoku-solver/',
+  base: 'https://github.com/jKnaebe/sudoku-solver/',
   plugins: [
     tailwindcss()
   ],
